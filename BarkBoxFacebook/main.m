d51e755e964bc0949f51055de948610964ae472d
github username <github email address>
2014-07-30 09:41:47 -0400

    Init commit, Parse and FB SDK

---
+++ BarkBoxFacebook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BarkBoxFacebook
+//
+//  Created by Troy Barrett on 7/30/14.
+//  Copyright (c) 2014 TMT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMTAppDelegate class]));
+    }
+}
